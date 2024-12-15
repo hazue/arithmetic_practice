5b790e9f55c7017b0cd87bc7d70aacb73ce83ff9
hazue <daishizennoha@gmail.com>
2024-12-15 22:30:04 +0800

    test adding 1 randomised arithmetic question

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-document.getElementsByTagName("main")[0].onload = function() {
-    newElem_Form = document.createElement("form");
-    this.appendNode(newElem_Form);
+function getRoundedInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function addQuestion(elemForm) {
+    elemQuestionContainer = document.createElement("div");
+    elemQuestionContainer.className = "question-container";
+
+    elemQuestionLabel = document.createElement("label");
+    intRandom1 = getRoundedInteger(11,99);
+    intRandom2 = getRoundedInteger(11,99);
+    elemQuestionLabel.innerText = intRandom1 + " + " + intRandom2 + " = ";
+    elemQuestionContainer.appendChild(elemQuestionLabel);
+
+    elemQuestionInput = document.createElement("input");
+    elemQuestionInput.type = "text";
+    elemQuestionContainer.appendChild(elemQuestionInput);
+
+    elemForm.appendChild(elemQuestionContainer);
     
+}
+
+function main(){
+    elem_Main = document.getElementsByTagName("main")[0];
+    elem_Form = document.createElement("form");
+    elem_Main.appendChild(elem_Form);
+    addQuestion(elem_Main);
+}
+
+
+document.onload = function(){
+    setTimeout( function(){
+        main();
+    }, 1000);
 }
