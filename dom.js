7e327b348caf452e866c75efd63535d25819de8a
hazue <daishizennoha@gmail.com>
2024-12-23 18:34:30 +0800

    added function to count timing; slight style up

--- dom.js
+++ dom.js
@@ -1,35 +1,37 @@
-function generateTimer(){
-    elem_Container = document.createElement("div");
-    elem_Container.className = "timer-container";
 
-    elem_toggleButton = document.createElement("button");
-    elem_toggleButton.innerText = ">";
-    elem_Container.appendChild(elem_toggleButton);
-
-    elem_Label = document.createElement("label");
-    elem_Label.innerText = "0.0";
-    elem_Container.appendChild(elem_Label);
-
-    return elem_Container;
-}
-
-function generateQuestionForm(){
-    elem_Form = document.createElement("form");
-
-    for(i=0;i<5;i++){
-        elem_Form.appendChild( generateQuestion() );
+function fnGenerateQuestionForm(isFirstLoad){
+    var elem_Form = null;
+    if(isFirstLoad===true){
+        console.log("generate form");
+        elem_Form = document.createElement("form");
+        elem_Form.id = "MainForm";
+        
+        document.getElementsByTagName("main")[0].appendChild(elem_Form);
+    }else{
+        //reset form
+        console.log("reset form");
+        elem_Form = document.getElementById("MainForm");
+        elem_Form.innerHTML = "";
     }
 
-    return elem_Form;
+
+    for(i=0;i<5;i++){
+        elem_Form.appendChild( fnGenerateQuestion(isFirstLoad) );
+    }
 }
 
-function generateQuestion() {
+function fnGenerateQuestion(isFirstLoad) {
     elemQuestionContainer = document.createElement("div");
     elemQuestionContainer.className = "question-container";
 
     elemQuestionLabel = document.createElement("label");
-    intRandom1 = getRoundedInteger(11,99);
-    intRandom2 = getRoundedInteger(11,99);
+    if(isFirstLoad){
+        intRandom1 = "OO";
+        intRandom2 = "XX";
+    }else{
+        intRandom1 = getRoundedInteger(11,99);
+        intRandom2 = getRoundedInteger(11,99);
+    }
     elemQuestionLabel.innerText = intRandom1 + " + " + intRandom2 + " = ";
     elemQuestionContainer.appendChild(elemQuestionLabel);
 
@@ -40,6 +42,50 @@ function generateQuestion() {
     return elemQuestionContainer;
 }
 
-function updateTimerText(intSeconds){
-    document.getElementById("timer").innerText = intSeconds;
+
+
+function fnGenerateTimer(){
+    elem_Container = document.createElement("div");
+    elem_Container.className = "timer-container";
+
+    elem_toggleButton = document.createElement("button");
+    elem_toggleButton.onclick = fnToggleTimer;
+    elem_toggleButton.innerText = ">";
+    elem_Container.appendChild(elem_toggleButton);
+
+    elem_Label = document.createElement("label");
+    elem_Label.id = "TimerText";
+    elem_Label.innerText = "0.0";
+    elem_Container.appendChild(elem_Label);
+
+    document.getElementsByTagName("main")[0].appendChild(elem_Container);
+}
+
+function fnToggleTimer(){
+    timer_stopped = !timer_stopped;
+    if(timer_stopped){
+        this.innerText = ">";
+        return
+    }
+
+    this.innerText = "||";
+    fnUpdateTimerText(0.0);
+
+    fnGenerateQuestionForm(false);
+}
+
+function fnUpdateTimerText(fltSeconds){
+    if (timer_stopped === true){
+        return;
+    }
+    
+    fltSeconds +=0.1;
+    setTimeout( function(){
+        fnUpdateTimerText(fltSeconds);
+    }, 93 );
+
+    
+    document.getElementById("TimerText").innerText = fltSeconds.toFixed(1);
+
+    
 }
--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 /* vvvvvv DECLARE ALL GLOBAL VARIABLE HERE vvvvvv */
-var timer_stopped = false;
+var timer_stopped = true;
 /* ^^^^^^ DECLARE ALL GLOBAL VARIABLE HERE ^^^^^^ */
 
-
-
-function timerCounting(intSeconds){
-    if (timer_stopped === true){
-        return;
-    }
-
-    setTimeout( function(){
-        intSeconds++;
-        updateTimerText(intSeconds);
-        timerCounting(intSeconds);
-    }, 1000 );
-}
-
-function stopTimer(){
-    timer_stopped = true;
-}
-
 function main(){
-    elem_Main = document.getElementsByTagName("main")[0];
-    elem_Main.appendChild( generateQuestionForm() );
-
-    elem_Main.appendChild( generateTimer() );
+    fnGenerateQuestionForm(true);   //generate this first so it'll always be on top of Timer
+    fnGenerateTimer();
 }
 
 
